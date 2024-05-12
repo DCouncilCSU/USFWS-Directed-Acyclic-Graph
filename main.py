022f589add78ef6342050dc12557cf8db661e3df
David Council <DCouncilCSU@gmail.com>
2024-05-12 13:54:38 +0000

    another test case

--- main.py
+++ main.py
@@ -16,20 +16,24 @@ class Graph:
     def __init__(self, adjacencyList):
         self.graph = adjacencyList
 
-    def get_longest_path(self, start, tempPathLength):
+    # Function to find longest path from given node
+    def getLongestPath(self, start, tempPathLength):
         if start not in self.visitedVertices:
             print("visiting: " + str(start))
             # mark as visited
             self.visitedVertices.append(start)
 
+            # if this is not a leaf node
             if len(self.graph[start]):
                 # increment temporary path length
                 tempPathLength += 1
                 # recurse through adjacent vertices
                 for adjVertex in self.graph[start]:
-                    self.get_longest_path(adjVertex, tempPathLength)
-            # if this is a leaf node, check if it is the longest path
+                    self.getLongestPath(adjVertex, tempPathLength)
+
+            # if this is a leaf node
             else:
+                # check if it is the longest path so far
                 if tempPathLength > self.pathLength:
                     self.pathLength = tempPathLength
 
@@ -37,8 +41,9 @@ class Graph:
         return self.pathLength
 
 # e.g. {vertex: [set of adjacent vertices], ...}
-adjacencyList = {0: [1, 2], 1: [3, 4], 2: [5], 3: [], 4: [5], 5: []}
+# adjacencyList = {0: [1, 2], 1: [3, 4], 2: [5], 3: [], 4: [5], 5: []}
+adjacencyList = {0: [1, 3], 1: [3, 4], 2: [3, 4, 5], 3: [5], 4: [5], 5: []}
 # construct Graph with adjacency list
 graph = Graph(adjacencyList)
 
-print(graph.get_longest_path(0, 0))
+print(graph.getLongestPath(2, 0))
