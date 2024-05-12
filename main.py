1e5d8bb081f43a72fa29005d509128b5d544a705
David Council <DCouncilCSU@gmail.com>
2024-05-12 13:45:09 +0000

    tempPathLength check seems to work

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 # USFWS Coding Challenge
 # David Council
 
+
 # Graph class, does not validate acyclic-ity
 class Graph:
     # Declare graph as a dictionary
     graph = {}
-    # Declare List to hold visited vertices
+    # Declare list to hold visited vertices
     visitedVertices = []
     # Declare path length starting at 0
     pathLength = 0
@@ -15,34 +16,29 @@ class Graph:
     def __init__(self, adjacencyList):
         self.graph = adjacencyList
 
-    def get_longest_path(self, start):
+    def get_longest_path(self, start, tempPathLength):
         if start not in self.visitedVertices:
             print("visiting: " + str(start))
             # mark as visited
             self.visitedVertices.append(start)
 
-            # increment path length
-            self.pathLength += 1
+            if len(self.graph[start]):
+                # increment temporary path length
+                tempPathLength += 1
+                # recurse through adjacent vertices
+                for adjVertex in self.graph[start]:
+                    self.get_longest_path(adjVertex, tempPathLength)
+            # if this is a leaf node, check if it is the longest path
+            else:
+                if tempPathLength > self.pathLength:
+                    self.pathLength = tempPathLength
 
-            # recurse through adjacent vertices
-            for adjVertex in self.graph[start]:
-                self.get_longest_path(adjVertex)
-
-        # return path length minus one,
-        # to account for 0 length at start
-        return self.pathLength - 1
-
+        # return path length
+        return self.pathLength
 
 # e.g. {vertex: [set of adjacent vertices], ...}
-adjacencyList = {
-    0: [1, 2],
-    1: [3, 4],
-    2: [5],
-    3: [],
-    4: [5],
-    5: []
-}
+adjacencyList = {0: [1, 2], 1: [3, 4], 2: [5], 3: [], 4: [5], 5: []}
 # construct Graph with adjacency list
 graph = Graph(adjacencyList)
 
-print(graph.get_longest_path(1))
+print(graph.get_longest_path(0, 0))
